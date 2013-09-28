2a23e953388b94eb521ee6eeabff2c5067509da0
Travis Beatty <travisby@gmail.com>
2013-09-28 18:44:52 -0400

    Added base ideas for jbehave steps

---
+++ circularlist/tests/CircularListSteps.java
@@ -0,0 +1,27 @@
+/**
+ * User: Travis Beatty
+ * Date: 9/28/13
+ * Time: 5:59 PM
+ */
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+public class CircularListSteps {
+
+    @Given("a circularlist size $size")
+    public void setList(int size) {
+        // TODO
+    }
+
+    @When("I get item $index")
+    public void getItem(int index) {
+        // TODO
+    }
+
+    @Then("it is item $index")
+    public void isEqualToItemAtIndex(int index) {
+        // TODO
+    }
+}
