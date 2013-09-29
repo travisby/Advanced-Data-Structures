7197d175a6ebcf446f8a73ebdc9dabaa1ab7a9ae
Travis Beatty <travisby@gmail.com>
2013-09-29 00:09:35 -0400

    Prepared implementors for List

---
+++ circularlist/src/list/LinkedList.java
@@ -0,0 +1,58 @@
+package list;
+
+import java.util.Iterator;
+
+/**
+ * User: Travis Beatty
+ * Date: 9/29/13
+ * Time: 12:08 AM
+ */
+public class LinkedList<E> implements List<E> {
+    @Override
+    public boolean add(E item) {
+        // TODO
+        return false;
+    }
+
+    @Override
+    public void add(int index, E item) {
+        // TODO
+
+    }
+
+    @Override
+    public void clear() {
+        // TODO
+
+    }
+
+    @Override
+    public E get(int index) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO
+        return false;
+    }
+
+    @Override
+    public E remove(int index) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public int size() {
+        // TODO
+        return 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        // TODO
+        return null;
+    }
+}
