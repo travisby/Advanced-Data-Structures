dc45c49c01207f9d029d0382739ad2bf6fb748aa
Travis Beatty <travisby@gmail.com>
2013-10-07 00:16:02 -0400

    Added simple ArrayList implementation

--- circularlist/src/list/ArrayList.java
+++ circularlist/src/list/ArrayList.java
@@ -8,51 +8,117 @@ import java.util.Iterator;
  * Time: 12:08 AM
  */
 public class ArrayList<E> implements List<E> {
-    @Override
-    public boolean add(E item) {
-        // TODO
-        return false;
+    private static final int STARTING_SIZE = 1;
+    private static final int EMPTY_SIZE_INDEX = -1;
+
+    private E[] array = null;
+    private int largestIndex = -1;
+
+    public ArrayList() {
+        clear();
     }
 
     @Override
-    public void add(int index, E item) throws IndexOutOfBoundsException  {
-        // TODO
+    public boolean add(E item) {
+        // Worry about resizing only when we are ALREADY full
+        // Premature optimization is the root of all evil!
+        // We do not know if the list will ever be added to again.  It would be unwise to resize it upon filling
+        if (largestIndex >= array.length - 1) {
+            copyToNewArray();
+        }
 
+        array[++largestIndex] = item;
+
+        return true;
+    }
+
+    @Override
+    public void add(int index, E item) throws IndexOutOfBoundsException {
+        if (index < 0 || index > largestIndex + 1) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        // Nice base case we can revert to
+        if (index == largestIndex + 1) {
+            add(item);
+            return;
+        }
+
+        // reindex around index
+        largestIndex++;
+        for (int i = largestIndex; i >= index; i--) {
+            array[i + 1] = array[i];
+        }
+
+        // finally add our item
+        array[index] = item;
     }
 
     @Override
     public void clear() {
-        // TODO
+        array = getArrayOfSize(STARTING_SIZE);
+        largestIndex = EMPTY_SIZE_INDEX;
 
     }
 
     @Override
     public E get(int index) throws IndexOutOfBoundsException {
-        // TODO
-        return null;
+        if (index < 0 || index > largestIndex) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return array[index];
     }
 
     @Override
     public boolean isEmpty() {
-        // TODO
-        return false;
+        return largestIndex == EMPTY_SIZE_INDEX;
     }
 
     @Override
     public E remove(int index) throws IndexOutOfBoundsException {
-        // TODO
-        return null;
+        E value = null;
+
+        if (index < 0 || index > largestIndex) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        value = array[index];
+
+        for (int i = index; i < largestIndex; i++) {
+            array[i] = array[i + 1];
+        }
+
+        largestIndex--;
+        return value;
     }
 
     @Override
     public int size() {
-        // TODO
-        return 0;
+        return largestIndex + 1;
     }
 
     @Override
     public Iterator<E> iterator() {
         // TODO
         return null;
+    }
+
+    private E[] getArrayOfSize(int size) {
+        // Thanks to http://stackoverflow.com/questions/529085/how-to-generic-array-creation
+        // I understood that Varkhan was trying to cast a new Array instance into one of a generic type
+        // Their version was much more... aroundabout?  We can complete the same mission by simply instantiating
+        // a new array in a normal fashion, rather than Array.newInstance()
+        return (E[]) (new Object[size]);  // Cast an Object array into a generics array
+    }
+
+    private void copyToNewArray() {
+        E[] newArray = getArrayOfSize(array.length * 2);
+
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+
+        array = newArray;
     }
 }
