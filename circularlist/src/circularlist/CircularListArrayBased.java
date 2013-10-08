b33eef8eca0af6cf1ddeeed002bb07a83b107ae3
Travis Beatty <travisby@gmail.com>
2013-10-08 09:44:41 -0400

    Added implementations for ACL

--- circularlist/src/circularlist/CircularListArrayBased.java
+++ circularlist/src/circularlist/CircularListArrayBased.java
@@ -2,8 +2,6 @@ package circularlist;
 
 import list.ArrayList;
 
-import java.util.Iterator;
-
 /**
  * User: Travis Beatty
  * Date: 9/28/13
@@ -15,100 +13,6 @@ public class CircularListArrayBased<E> extends AbstractCircularList<E> {
 
     public CircularListArrayBased() {
 
-        new ArrayList<E>();
-    }
-
-    /**
-     * Determines whether a list is empty.
-     *
-     * @return true if the list is empty, otherwise false
-     */
-    @Override
-    public boolean isEmpty() {
-        // TODO
-        return false;
-    }
-
-    /**
-     * Determines the length of a list.
-     *
-     * @return the number of elements in the list without wrapping
-     */
-    @Override
-    public int size() {
-        // TODO
-        return 0;
-    }
-
-    /**
-     * Removes all elements from the list.
-     */
-    @Override
-    public void clear() {
-        // TODO
-
-    }
-
-    /**
-     * Adds a new item to the end of the list.
-     *
-     * @param item the new item to add
-     * @return true if the list was modified
-     */
-    @Override
-    public boolean add(E item) {
-        // TODO
-        return false;
-    }
-
-    /**
-     * Adds a new item to the list at position index.
-     * Other items are shifted, not overwritten.
-     *
-     * @param index where to add the new item
-     * @param item  the new item to add
-     * @throws IndexOutOfBoundsException if index is negative
-     */
-    @Override
-    public void add(int index, E item) throws IndexOutOfBoundsException {
-        // TODO
-
-    }
-
-    /**
-     * Remove and return the item at the given index.
-     *
-     * @param index the position of the item to remove
-     * @return the item that was removed
-     * @throws IndexOutOfBoundsException if index is negative
-     */
-    @Override
-    public E remove(int index) throws IndexOutOfBoundsException {
-        // TODO
-        return null;
-    }
-
-    /**
-     * Retrieve the item at the given index without altering the list.
-     *
-     * @param index the position of the item to retrieve
-     * @return the item at position index
-     * @throws IndexOutOfBoundsException if index is negative
-     */
-    @Override
-    public E get(int index) throws IndexOutOfBoundsException {
-        // TODO
-        return null;
-    }
-
-    /**
-     * Generate an iterator for the list.  The iterator should
-     * visit the items in a circular pattern.  As long as the
-     * list is not empty, it should never stop.
-     */
-    @Override
-    public Iterator<E> iterator() {
-        // TODO
-        return null;
+        list = new ArrayList<E>();
     }
 }
--- circularlist/src/circularlist/CircularListReferenceBased.java
+++ circularlist/src/circularlist/CircularListReferenceBased.java
@@ -1,6 +1,6 @@
 package circularlist;
 
-import java.util.Iterator;
+import list.LinkedList;
 
 /**
  * User: Travis Beatty
@@ -10,98 +10,7 @@ import java.util.Iterator;
 @SuppressWarnings({"Annotation", "ClassWithoutLogger", "PublicMethodWithoutLogging", "SuppressionAnnotation", "LocalCanBeFinal", "PublicConstructor"})
 public class CircularListReferenceBased<E> extends AbstractCircularList<E> {
 
-
-    /**
-     * Determines whether a list is empty.
-     *
-     * @return true if the list is empty, otherwise false
-     */
-    @Override
-    public boolean isEmpty() {
-        // TODO
-        return false;
-    }
-
-    /**
-     * Determines the length of a list.
-     *
-     * @return the number of elements in the list without wrapping
-     */
-    @Override
-    public int size() {
-        // TODO
-        return 0;
-    }
-
-    /**
-     * Removes all elements from the list.
-     */
-    @Override
-    public void clear() {
-        // TODO
-
-    }
-
-    /**
-     * Adds a new item to the end of the list.
-     *
-     * @param item the new item to add
-     * @return true if the list was modified
-     */
-    @Override
-    public boolean add(E item) {
-        // TODO
-        return false;
-    }
-
-    /**
-     * Adds a new item to the list at position index.
-     * Other items are shifted, not overwritten.
-     *
-     * @param index where to add the new item
-     * @param item  the new item to add
-     * @throws IndexOutOfBoundsException if index is negative
-     */
-    @Override
-    public void add(int index, E item) throws IndexOutOfBoundsException {
-        // TODO
-
-    }
-
-    /**
-     * Remove and return the item at the given index.
-     *
-     * @param index the position of the item to remove
-     * @return the item that was removed
-     * @throws IndexOutOfBoundsException if index is negative
-     */
-    @Override
-    public E remove(int index) throws IndexOutOfBoundsException {
-        // TODO
-        return null;
-    }
-
-    /**
-     * Retrieve the item at the given index without altering the list.
-     *
-     * @param index the position of the item to retrieve
-     * @return the item at position index
-     * @throws IndexOutOfBoundsException if index is negative
-     */
-    @Override
-    public E get(int index) throws IndexOutOfBoundsException {
-        // TODO
-        return null;
-    }
-
-    /**
-     * Generate an iterator for the list.  The iterator should
-     * visit the items in a circular pattern.  As long as the
-     * list is not empty, it should never stop.
-     */
-    @Override
-    public Iterator<E> iterator() {
-        // TODO
-        return null;
+    public CircularListReferenceBased() {
+        list = new LinkedList<E>();
     }
 }
