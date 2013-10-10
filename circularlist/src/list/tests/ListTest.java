ccf45f98867c54bffbdc5ea0844507940b852765
Travis Beatty <travisby@gmail.com>
2013-10-10 19:02:13 -0400

    Cleaned up LT

--- circularlist/src/list/tests/ListTest.java
+++ circularlist/src/list/tests/ListTest.java
@@ -1,46 +1,42 @@
 package list.tests;
 
+import circularlist.tests.CircularListSteps;
 import de.codecentric.jbehave.junit.monitoring.JUnitReportingRunner;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
-import org.jbehave.core.embedder.StoryControls;
-import org.jbehave.core.failures.FailingUponPendingStep;
-import org.jbehave.core.junit.JUnitStory;
-import org.jbehave.core.reporters.StoryReporterBuilder;
+import org.jbehave.core.junit.JUnitStories;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
 import org.junit.runner.RunWith;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Travis on 10/6/13.
  */
-
 @RunWith(JUnitReportingRunner.class)
+public class ListTest extends JUnitStories {
+    private Configuration configuration;
 
-public class ListTest extends JUnitStory {
+    public ListTest() {
+        super();
+        configuration = new MostUsefulConfiguration();
+    }
+
+    @Override
+    protected List<String> storyPaths() {
+        return Arrays.asList("list/tests/list_test.story");
+
+    }
+
     @Override
     public Configuration configuration() {
-        return new MostUsefulConfiguration()
-                .useStoryReporterBuilder(
-                        new StoryReporterBuilder()
-                                .withDefaultFormats()
-                                .withFailureTrace(true)
-                                .withMultiThreading(false)
-                )
-                .usePendingStepStrategy(new FailingUponPendingStep())
-                .useStoryControls(
-                        new StoryControls()
-                                .doSkipScenariosAfterFailure(true)
-                                .doResetStateBeforeScenario(true)
-                                .doResetStateBeforeStory(true)
-                                .doSkipBeforeAndAfterScenarioStepsIfGivenStory(false)
-                )
-                ;
+        return configuration;
     }
 
     @Override
     public InjectableStepsFactory stepsFactory() {
-        return new InstanceStepsFactory(configuration(), new ListSteps());
-
+        return new InstanceStepsFactory(configuration(), new CircularListSteps());
     }
 }
