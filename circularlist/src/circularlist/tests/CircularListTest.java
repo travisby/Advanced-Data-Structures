462297b155835af16c6f89bd0500e755d3cc5c27
Travis Beatty <travisby@gmail.com>
2013-10-10 18:56:08 -0400

    Updated test runner to use the MostUsefulConfig

--- circularlist/src/circularlist/tests/CircularListTest.java
+++ circularlist/src/circularlist/tests/CircularListTest.java
@@ -1,28 +1,15 @@
 package circularlist.tests;
 
-import com.thoughtworks.paranamer.NullParanamer;
 import de.codecentric.jbehave.junit.monitoring.JUnitReportingRunner;
 import org.jbehave.core.configuration.Configuration;
-import org.jbehave.core.embedder.EmbedderControls;
-import org.jbehave.core.embedder.StoryControls;
-import org.jbehave.core.failures.FailingUponPendingStep;
-import org.jbehave.core.failures.RethrowingFailure;
-import org.jbehave.core.i18n.LocalizedKeywords;
-import org.jbehave.core.io.AbsolutePathCalculator;
-import org.jbehave.core.io.LoadFromClasspath;
-import org.jbehave.core.io.UnderscoredCamelCaseResolver;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.junit.JUnitStories;
-import org.jbehave.core.parsers.RegexPrefixCapturingPatternParser;
-import org.jbehave.core.parsers.RegexStoryParser;
-import org.jbehave.core.reporters.FreemarkerViewGenerator;
-import org.jbehave.core.reporters.PrintStreamStepdocReporter;
-import org.jbehave.core.reporters.StoryReporterBuilder;
-import org.jbehave.core.steps.*;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.InstanceStepsFactory;
 import org.junit.runner.RunWith;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 
 /**
  * Created by Travis on 10/6/13.
@@ -34,49 +21,7 @@ public class CircularListTest extends JUnitStories {
 
     public CircularListTest() {
         super();
-        configuration = new Configuration() {
-        };
-
-        // configuration.doDryRun(false); "no dry run" is implicit by using
-        // default StoryControls
-
-        // configuration.useDefaultStoryReporter(new ConsoleOutput());
-        // deprecated -- rather use StoryReportBuilder
-
-
-        configuration.useFailureStrategy(new RethrowingFailure());
-        configuration.useKeywords(new LocalizedKeywords(Locale.ENGLISH));
-        configuration.usePathCalculator(new AbsolutePathCalculator());
-        configuration.useParameterControls(new ParameterControls());
-        configuration.useParameterConverters(new ParameterConverters());
-        configuration.useParanamer(new NullParanamer());
-        configuration.usePendingStepStrategy(new FailingUponPendingStep());
-        configuration.useStepCollector(new MarkUnmatchedStepsAsPending());
-        configuration.useStepdocReporter(new PrintStreamStepdocReporter());
-        configuration.useStepFinder(new StepFinder());
-        configuration.useStepMonitor(new SilentStepMonitor());
-        configuration
-                .useStepPatternParser(new RegexPrefixCapturingPatternParser());
-        configuration.useStoryControls(new StoryControls());
-        configuration.useStoryLoader(new LoadFromClasspath());
-        configuration.useStoryParser(new RegexStoryParser(configuration
-                .keywords()));
-        configuration.useStoryPathResolver(new UnderscoredCamelCaseResolver());
-        configuration.useStoryReporterBuilder(new StoryReporterBuilder());
-        configuration.useViewGenerator(new FreemarkerViewGenerator());
-
-
-        EmbedderControls embedderControls = configuredEmbedder()
-                .embedderControls();
-        embedderControls.doBatch(false);
-        embedderControls.doGenerateViewAfterStories(true);
-        embedderControls.doIgnoreFailureInStories(false);
-        embedderControls.doIgnoreFailureInView(false);
-        embedderControls.doSkip(false);
-        embedderControls.doVerboseFailures(false);
-        embedderControls.doVerboseFiltering(false);
-        embedderControls.useStoryTimeoutInSecs(300);
-        embedderControls.useThreads(1);
+        configuration = new MostUsefulConfiguration();
     }
 
     @Override
