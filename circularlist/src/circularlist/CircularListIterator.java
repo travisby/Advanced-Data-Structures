c177ab87fc03bcb8bd3e88fc6d0cea6095a2d1a8
Travis Beatty <travisby@gmail.com>
2013-10-11 09:16:11 -0400

    Implemented CL Iterator

--- circularlist/src/circularlist/CircularListIterator.java
+++ circularlist/src/circularlist/CircularListIterator.java
@@ -6,21 +6,32 @@ import java.util.Iterator;
  * Created by Travis on 10/11/13.
  */
 public class CircularListIterator<E> implements Iterator<E> {
+
+    private CircularList<E> list;
+    private int currentIndex;
+
     @Override
     public boolean hasNext() {
-        return false;
+
+        // The only time we cannot iterate is if our list is empty
+        if (list.size() == 0) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public E next() {
-        return null;
+        return list.get(currentIndex++);
     }
 
     @Override
     public void remove() {
-
+        // This page intentionally left blank
     }
 
     public CircularListIterator(CircularList<E> circularList) {
+        list = circularList;
+        currentIndex = 0;
     }
 }
