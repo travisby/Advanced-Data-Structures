8d1740b96a5d4b95b722a6bd503c5c2563616b7a
Travis Beatty <travisby@gmail.com>
2013-11-14 14:27:19 -0500

    Implemented Person methods

--- vacsys/src/vacsys/Person.java
+++ vacsys/src/vacsys/Person.java
@@ -6,19 +6,34 @@ package vacsys;
  * @author Travis
  */
 public class Person {
-    public String getName() {
-        // TODO
-        return "";
-    }
 
-    public int age() {
-        // TODO
-        return 0;
-    }
-
+    protected String name;
+    protected int age;
     // We use a String for zipcodes because they seldom need numerical ordering, and will be smaller
+    protected String zip;
+
+    /**
+     * Gets the name of this person
+     *
+     * @return this's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gets the age of this person
+     * @return this's age
+     */
+    public int age() {
+        return age;
+    }
+
+    /**
+     * Gets the zip code where this person lives
+     * @return this's zip code
+     */
     public String zip() {
-        // TODO
-        return "";
+        return zip;
     }
 }
