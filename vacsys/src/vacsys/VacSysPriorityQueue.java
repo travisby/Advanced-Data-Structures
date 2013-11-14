686850b2cf86b245e8ee9cb41bc9bcad2c000a6f
Travis Beatty <travisby@gmail.com>
2013-11-14 14:20:19 -0500

    Used VacSysHeap & VacSysPriorityQueue to implement a Collection

--- vacsys/src/vacsys/VacSysPriorityQueue.java
+++ vacsys/src/vacsys/VacSysPriorityQueue.java
@@ -1,9 +1,10 @@
 package vacsys;
 
+import java.util.Collection;
+
 /**
  * Priority Queue interface for efficiently ordering patients
- *
  * @author Travis
  */
-public interface VacSysPriorityQueue {
+public interface VacSysPriorityQueue<E> extends Collection<E> {
 }
