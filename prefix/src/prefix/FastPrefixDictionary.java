0b5c3084ec9482a62a814a6ae8bd60f3a94518e0
Travis Beatty <travisby@gmail.com>
2013-12-09 15:47:49 -0500

    Added boilerplate for file reading

--- prefix/src/prefix/FastPrefixDictionary.java
+++ prefix/src/prefix/FastPrefixDictionary.java
@@ -1,10 +1,27 @@
 package prefix;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 /**
  * Created by travis on 12/9/13.
  */
 public class FastPrefixDictionary implements PrefixDictionary {
-    public FastPrefixDictionary(String arg) {
+
+    public FastPrefixDictionary(String filename) {
+
+        try {
+            BufferedReader file = new BufferedReader(new FileReader(filename));
+            String line;
+            String[] lineList;
+            while ((line = file.readLine()) != null) {
+                // TODO build from file
+            }
+            file.close();
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
     }
 
     @Override
