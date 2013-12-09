85758d1b3aa544808fc7d18ed2b6a233fb53716c
Travis Beatty <travisby@gmail.com>
2013-12-09 15:54:27 -0500

    Added PrefixHashMap.getValue()

--- prefix/src/prefix/PrefixHashMap.java
+++ prefix/src/prefix/PrefixHashMap.java
@@ -6,4 +6,14 @@ import java.util.HashMap;
  * Created by travis on 12/9/13.
  */
 public class PrefixHashMap<T, Y> extends HashMap {
+    private int value;
+
+    public PrefixHashMap(int myValue) {
+        super(26);
+        value = myValue;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
